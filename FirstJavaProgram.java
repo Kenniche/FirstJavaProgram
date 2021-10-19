615af6984886a469dbfb9a5b31d06c78ae131a9a
Kenniche <kenniche23@gmail.com>
2021-10-19 07:26:41 -0600

    initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+    public static void main (String []args){
+        System.out.println("My name is Kenneth Chaves");
+        System.out.println("I am 45 years old");
+        System.out.println("My hometown is Heredia, CR");
+
+    }
+}
