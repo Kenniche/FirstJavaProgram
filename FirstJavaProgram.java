b8a3b906bd12d76147c283f26813c7c05b41a666
Kenniche <kenniche23@gmail.com>
2021-10-19 09:46:04 -0600

    add code

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,8 +1,13 @@
 public class FirstJavaProgram {
     public static void main (String []args){
-        System.out.println("My name is Kenneth Chaves");
+        System.out.println("My name is Kenneth");
         System.out.println("I am 45 years old");
         System.out.println("My hometown is Heredia, CR");
 
+        String name = "Kenneth,";
+        String home = "Heredia, CR";
+        int age = 45;
+        System.out.println("My name is "+ name + " I am " + age + " years old," + " My Hometown is " + home);
     }
 }
+
